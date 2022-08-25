f7a3e57622d71fbe60fa7eba22a1fbb124fe77f1
RZyiczhan <2088915035@qq.com>
2022-08-25 23:54:21 +1000

    change class to className

--- FrontEnd/food-meal-ordering-system/src/pages/menu.js
+++ FrontEnd/food-meal-ordering-system/src/pages/menu.js
@@ -1,83 +1,85 @@
 import React from "react";
 
-function accessEditingMode(){
-    let editBtn =  document.getElementById("editBtn");
-    let addBtns = document.getElementsByClassName("addDishBtn");
-    let delBtns = document.getElementsByClassName("delDishBtn")
-    if (editBtn.innerHTML == "Edit"){
-        editBtn.innerHTML = "Editing";
-        document.getElementById("add_more_button").style.display = "flex";
-        
-        for(let i = 0; i < addBtns.length; i++){
-            addBtns[i].style.display = "none"
-            delBtns[i].style.display = "block"
-        }
-
-    } else {
-        editBtn.innerHTML = "Edit";
-        document.getElementById("add_more_button").style.display = "None";
-
-        for(let i = 0; i < addBtns.length; i++){
-            addBtns[i].style.display = "block"
-            delBtns[i].style.display = "none"
-        }
-    }
-}
 
 export default function menu(){
+
+    function accessEditingMode(){
+        let editBtn =  document.getElementById("editBtn");
+        let addBtns = document.getElementsByClassName("addDishBtn");
+        let delBtns = document.getElementsByClassName("delDishBtn")
+        if (editBtn.innerHTML == "Edit"){
+            editBtn.innerHTML = "Editing";
+            document.getElementById("add_more_button").style.display = "flex";
+            
+            for(let i = 0; i < addBtns.length; i++){
+                addBtns[i].style.display = "none"
+                delBtns[i].style.display = "block"
+            }
+    
+        } else {
+            editBtn.innerHTML = "Edit";
+            document.getElementById("add_more_button").style.display = "None";
+    
+            for(let i = 0; i < addBtns.length; i++){
+                addBtns[i].style.display = "block"
+                delBtns[i].style.display = "none"
+            }
+        }
+    }
+
     return (
         <div>
-            <div class="menu_head">
+            <div className="menu_head">
                 <img id="menupic" src="./res/images/menu_pic_temp.png" alt="menu picture" />
-                <img class="logo" src="./res/images/projectIcon.png" alt="logo" />
+                <img className="logo" src="./res/images/projectIcon.png" alt="logo" />
                 <div id="menuword">menu</div>
             </div>
 
-            <div class="nav">
+            <div className="nav">
                 <input name="returnBtn" type="image" src="./res/images/arrow.png" alt="return button icon" />
                 <span>Table No.</span>
             </div>
-            <div class="inner_menu_container">
+            <div className="inner_menu_container">
                 <div>
                     <h2>Chicken</h2>
                 </div>
                 <button id="editBtn" type="button" onClick={accessEditingMode}>Edit</button>
             </div>
-            <div class="foodunit">
+            <div className="foodunit">
                 <hr />
-                <div class="foodbox">
+                <div className="foodbox">
                     <img src="./res/images/Big_Mac_Chicken_Burger.png" alt="Big_Mac_Chicken_Burger_picture" />
                     
-                    <div class="textbox">
-                        <div class="burger_name">Big_Mac_Chicken_Burger</div>
-                        <div class="burger_desc">Huge chicken chop, fresh lettuce.</div>
-                        <div class="burger_others">
-                            <div class="burger_joules">2880KJ</div>
-                            <div class="burger_price">$13.80</div>
+                    <div className="textbox">
+                        <div className="burger_name">Big_Mac_Chicken_Burger</div>
+                        <div className="burger_desc">Huge chicken chop, fresh lettuce.</div>
+                        <div className="burger_others">
+                            <div className="burger_joules">2880KJ</div>
+                            <div className="burger_price">$13.80</div>
                         </div>
                     </div>
 
-                    <input class="addDishBtn" name="addDishBtn" type="image" src="./res/images/add_button.png" alt="add button icon" />
-                    <input class="delDishBtn" name="delDishBtn" type="image" src="./res/images/delete_btn.png" alt="delete button icon" />
+                    <input className="addDishBtn" name="addDishBtn" type="image" src="./res/images/add_button.png" alt="add button icon" />
+                    <input className="delDishBtn" name="delDishBtn" type="image" src="./res/images/delete_btn.png" alt="delete button icon" />
                 </div>
             </div>
-            <div class="foodunit">
+            <div className="foodunit">
                 <hr />
-                <div class="foodbox">
+                <div className="foodbox">
                     <img src="./res/images/Double_Drumstick_Burger.png" alt="Double_Drumstick_Burger_picture" />
                     
-                    <div class="textbox">
-                        <div class="burger_name">Double_Drumstick_Burger</div>
-                        <div class="burger_desc">Two chicken steaks, 
+                    <div className="textbox">
+                        <div className="burger_name">Double_Drumstick_Burger</div>
+                        <div className="burger_desc">Two chicken steaks, 
                             fresh tomatoes, lettuce.</div>
-                        <div class="burger_others">
-                            <div class="burger_joules">3150KJ</div>
-                            <div class="burger_price">$16.80</div>
+                        <div className="burger_others">
+                            <div className="burger_joules">3150KJ</div>
+                            <div className="burger_price">$16.80</div>
                         </div>
                     </div>
 
-                    <input class="addDishBtn" name="addDishBtn" type="image" src="./res/images/add_button.png" alt="add button icon" />
-                    <input class="delDishBtn" name="delDishBtn" type="image" src="./res/images/delete_btn.png" alt="delete button icon" />
+                    <input className="addDishBtn" name="addDishBtn" type="image" src="./res/images/add_button.png" alt="add button icon" />
+                    <input className="delDishBtn" name="delDishBtn" type="image" src="./res/images/delete_btn.png" alt="delete button icon" />
                 </div>
             </div>
             <button id="add_more_button">Add more dish</button>
