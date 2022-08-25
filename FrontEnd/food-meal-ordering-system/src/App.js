f71b755c59984c67fba3e0c04205722f74d43db0
RZyiczhan <2088915035@qq.com>
2022-08-25 17:53:55 +1000

    finish html template

--- FrontEnd/food-meal-ordering-system/src/App.js
+++ FrontEnd/food-meal-ordering-system/src/App.js
@@ -1,22 +1,8 @@
 import React, { useState} from "react";
-// import TodoList from "./TodoList";
-
-// function App() {
-//   const [todos, setTodos] = useState([{id : 1, name : "Todos 1", complete : false}])
-//   return (
-//     <>
-//     <TodoList todos = {todos}/> // this is jsx, allow us to enbedden component
-//     </>  
-//     //注意，一次只能return一个html element集合或者jsx element
-//     //所以我们把他们放到同一个empty的<></>里面视为return一个东西
-//   );
-// }
-
+import Menu from "./pages/menu"
 function App() {
     return (
-        <>
-        <img src="../res/images/menu-pic-temp"></img>
-        </>
+        <Menu />
     );
 }
 
