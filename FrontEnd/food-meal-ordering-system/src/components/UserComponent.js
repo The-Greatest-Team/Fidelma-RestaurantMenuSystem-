e70ed87236a0fce0bea148c3d6dc16bd385a3935
zhzizhou <zhzizhou@gmail.com>
2022-08-26 21:29:26 +1000

    updated origin path

--- FrontEnd/food-meal-ordering-system/src/components/UserComponent.js
+++ FrontEnd/food-meal-ordering-system/src/components/UserComponent.js
@@ -5,30 +5,19 @@ class UserComponent extends React.Component{
 
     constructor(props){
         super(props)
-        this.state = {user : []}
+
+        this.state = {users : []}
     }
 
     componentDidMount(){
         UserService.getUsers().then((respond) => {
             this.setState({users : respond.data })
-            console.log(respond.data)
+            console.log(this.state.users);
         });
-        console.log(this.state);
     }
 
     render(){
-        return(
-            <div>{
-                this.state.user.map(
-                    user => {
-                        <>
-                        <div>{user.firstname}</div>
-                        <div>{user.id} </div>
-                        </>
-                    }
-                )
-                }
-            </div>
+        return(null
         );
     }
 
