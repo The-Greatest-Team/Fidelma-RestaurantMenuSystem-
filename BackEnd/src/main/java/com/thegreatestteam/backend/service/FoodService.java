c62203ad95602e2287522d69b48366ab4c465eae
Yu Hin Li <yuhinl@student.unimelb.edu.au>
2022-08-30 16:49:57 +1000

    add check Availability

--- BackEnd/src/main/java/com/thegreatestteam/backend/service/FoodService.java
+++ BackEnd/src/main/java/com/thegreatestteam/backend/service/FoodService.java
@@ -1,25 +1,42 @@
 package com.thegreatestteam.backend.service;
 
 import com.thegreatestteam.backend.model.Food;
+import com.thegreatestteam.backend.model.Ingredient;
 import com.thegreatestteam.backend.repository.FoodRepository;
-import org.bson.BsonBinarySubType;
-import org.bson.types.Binary;
+import com.thegreatestteam.backend.repository.IngredientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 
 @Service
 public class FoodService {
-
     @Autowired
     private final FoodRepository foodRepository;
+    @Autowired
+    private final IngredientRepository ingredientRepository;
 
-    public FoodService(FoodRepository foodRepository) {
+    public FoodService(IngredientRepository ingredientRepository, FoodRepository foodRepository) {
         this.foodRepository = foodRepository;
+        this.ingredientRepository = ingredientRepository;
     }
 
+    public boolean checkAvailability(Food food){
+        boolean available = true;
+
+        for(String ingredientName: food.getComponents().keySet()){
+            Ingredient ingredient = ingredientRepository.findByName(ingredientName);
+            Integer currentQuantity = Integer.valueOf(ingredient.getQuantity());
+            Integer requiredQuantity = food.getComponents().get(ingredientName).intValue();
+
+            if(currentQuantity - requiredQuantity < 0 ){
+                System.out.println("currentQuantity: " + currentQuantity);
+                System.out.println("requiredQuantity: " + requiredQuantity);
+
+                available = false;
+            }
+        }
+
+        return available;
+    }
 
 //    public void addImage(Food food, MultipartFile file) throws IOException {
 //        food.setImage( new Binary(BsonBinarySubType.BINARY, file.getBytes()));
