0133ab948fefef2f08124c68ce6182b95d94e394
YuHinLi <yuhinl@student.unimelb.edu.au>
2022-09-05 23:22:22 +1000

    Add Staff Service

---
+++ BackEnd/src/main/java/com/thegreatestteam/backend/service/StaffService.java
@@ -0,0 +1,22 @@
+package com.thegreatestteam.backend.service;
+
+import com.thegreatestteam.backend.model.Staff;
+import com.thegreatestteam.backend.repository.StaffRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StaffService {
+
+    private final StaffRepository staffRepository;
+
+    @Autowired
+    public StaffService(StaffRepository staffRepository) {
+        this.staffRepository = staffRepository;
+    }
+
+    //Get Staff with Id
+    public Staff getAllStaffWithID(String Id){
+        return staffRepository.findStaffById(Id);
+    }
+}
