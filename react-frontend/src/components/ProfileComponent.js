36eaaeccab9b7d5fedad47518f0ff549e00310ed
Steph-PO8 <wendiying0311@gmail.com>
2022-09-06 15:21:04 +1000

    Update profile component

--- react-frontend/src/components/ProfileComponent.js
+++ react-frontend/src/components/ProfileComponent.js
@@ -15,7 +15,80 @@ class ProfileComponent extends React.Component{
         return(
             <>
                 <div>
-                    <h1>profile</h1>
+                    <div className = "ingredientHeader">
+                        <img className = "backButton" src = "/res/images/back.svg" onClick={()=>window.location.href="/staff/dashboard"}/>
+                    </div>
+                    <div className = "profileTitle">
+                        <h3>Edit Profile</h3>
+                        <hr className = "titleSeparateLine"/>
+                    </div>
+                    <div>
+                        <img className = "profileImg" src = "/res/images/staff.svg" alt = "staff" />
+                    </div>
+                    <div className = "profileContent">
+                        <div className = "left">
+                            <h4><strong>Name</strong></h4>
+                        </div>
+                        <div className = "right">
+                            <h4>Wendi YING</h4>
+                        </div>
+                    </div>    
+                    <hr className = "profileSeparateLine"/>
+                    <div className = "profileContent">
+                        <div className = "left">
+                            <h4><strong>ID</strong></h4>
+                        </div>
+                        <div className = "right">
+                            <h4>1000001</h4>
+                        </div>
+                    </div>  
+                    <hr className = "profileSeparateLine"/>
+                    <div className = "profileContent">
+                        <div className = "left">
+                            <h4><strong>Gender</strong></h4>
+                        </div>
+                        <div className = "right">
+                            <h4>Male</h4>
+                        </div>
+                    </div>  
+                    <hr className = "profileSeparateLine"/>
+                    <div className = "profileContent">
+                        <div className = "left">
+                            <h4><strong>Date of Birth</strong></h4>
+                        </div>
+                        <div className = "right">
+                            <h4>11/03/2002</h4>
+                        </div>
+                    </div>  
+                    <hr className = "profileSeparateLine"/>
+                    <div className = "profileContent">
+                        <div className = "left">
+                            <h4><strong>Contact Number</strong></h4>
+                        </div>
+                        <div className = "right">
+                            <h4>0412345678</h4>
+                        </div>
+                    </div>  
+                    <hr className = "profileSeparateLine"/>
+                    <div className = "profileContent">
+                        <div className = "left">
+                            <h4><strong>Email</strong></h4>
+                        </div>
+                        <div className = "right">
+                            <h4>adc@gmail.com</h4>
+                        </div>
+                    </div>  
+                    <hr className = "profileSeparateLine"/>
+                    <div className = "profileContent">
+                        <div className = "left">
+                            <h4><strong>Address</strong></h4>
+                        </div>
+                        <div className = "right">
+                            <h4>Parkville VIC 3010</h4>
+                        </div>
+                    </div>  
+                    <hr className = "profileSeparateLine"/>
+
                 </div>
             </>
         );
