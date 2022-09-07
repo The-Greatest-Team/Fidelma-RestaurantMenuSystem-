1673b41b1a18e66efd69e21a190f583865a83232
YuHinLi <yuhinl@student.unimelb.edu.au>
2022-09-07 17:51:59 +1000

    Update FoodController.java

--- BackEnd/src/main/java/com/thegreatestteam/backend/controller/FoodController.java
+++ BackEnd/src/main/java/com/thegreatestteam/backend/controller/FoodController.java
@@ -55,10 +55,16 @@ public class FoodController {
         foodService.addFood(food);
     }
 
+//    @GetMapping("/staff/menu/edit/{id}")
+//    public Food getEditDish(@PathVariable String id){
+//        return foodService.getFoodById(id);
+//    }
+
     @GetMapping("/staff/menu/edit/{id}")
-    public Food getEditDish(@PathVariable String id){
-        return foodService.getFoodById(id);
+    public List<Ingredient> getEditDish(){
+        return foodService.getAllIngredient();
     }
+
 
     @PutMapping("/staff/menu/edit/{id}")
     public void updateDish(@RequestBody Food newFood, @PathVariable String id){
