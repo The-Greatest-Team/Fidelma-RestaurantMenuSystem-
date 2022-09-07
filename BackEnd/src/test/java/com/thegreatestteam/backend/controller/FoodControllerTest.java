290898c184785daa1caf4bfb2a5c3cc14e37628b
YuHinLi <yuhinl@student.unimelb.edu.au>
2022-09-07 20:39:59 +1000

    Add Integration Test for all GET method for food controller

--- BackEnd/src/test/java/com/thegreatestteam/backend/controller/FoodControllerTest.java
+++ BackEnd/src/test/java/com/thegreatestteam/backend/controller/FoodControllerTest.java
@@ -3,43 +3,75 @@ package com.thegreatestteam.backend.controller;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
-@WebMvcTest(FoodController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class FoodControllerTest {
 
     @Autowired
     private MockMvc mcv;
+    private RequestBuilder request;
 
     @Test
     void getChickenFood() throws Exception {
-        RequestBuilder request = MockMvcRequestBuilders.get("/staff");
-        try {
-            MvcResult result = mcv.perform(request).andReturn();
-            System.out.println(result.getResponse().getContentAsString());
-        } catch (Exception e) {
-            throw new RuntimeException("Fail to get result");
-        }
+        request = MockMvcRequestBuilders.get("/staff/menu/chicken");
+        mcv.perform(request).
+                andDo(print()).
+                andExpect(status().isOk());
     }
 
     @Test
-    void getBeefFood() {
+    void getBeefFood() throws Exception {
+        request = MockMvcRequestBuilders.get("/staff/menu/beef");
+        mcv.perform(request).
+                andDo(print()).
+                andExpect(status().isOk());
     }
 
     @Test
-    void getSideFood() {
+    void getSideFood() throws Exception {
+        request = MockMvcRequestBuilders.get("/staff/menu/side");
+        mcv.perform(request).
+                andDo(print()).
+                andExpect(status().isOk());
     }
 
     @Test
-    void getChipFood() {
+    void getChipFood() throws Exception {
+        request = MockMvcRequestBuilders.get("/staff/menu/chip");
+        mcv.perform(request).
+                andDo(print()).
+                andExpect(status().isOk());
+    }
+
+    @Test
+    void getNewDishIngredient() throws Exception {
+        request = MockMvcRequestBuilders.get("/staff/menu/newDish");
+        mcv.perform(request).
+                andDo(print()).
+                andExpect(status().isOk());
+    }
+
+    @Test
+    void getEditDish() throws Exception {
+        request = MockMvcRequestBuilders.get("/staff/menu/edit/63184a91169a5c4d695cae61");
+        mcv.perform(request).
+                andDo(print()).
+                andExpect(status().isOk());
     }
 }
