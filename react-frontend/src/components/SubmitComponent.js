982117624e9083e5d2a8e1a0cc12de978f6a906b
Steph-PO8 <wendiying0311@gmail.com>
2022-09-12 14:58:55 +1000

    Update submit page

--- react-frontend/src/components/SubmitComponent.js
+++ react-frontend/src/components/SubmitComponent.js
@@ -15,6 +15,23 @@ class SubmitComponent extends React.Component{
         return(
             <>
                 <div>
+                    <div className = "submitHead">
+                        <h4>Fidelma</h4>
+                    </div>
+                    <div className = "orderHead">
+                        <img className="logo" src="/res/images/projectIcon.png" alt="logo" />
+                    </div>
+                    <div className = "thanks">
+                        <h5>Thanks for shopping in Fidelma</h5>
+                    </div>
+                    <div className = "orderHead">
+                        <img className="logo" src="/res/images/submit.png" />
+                    </div>
+                    <div className = "success">
+                        <h5>Your order is submitted to reception</h5>
+                        <h5>Please go to reception and pay you order!</h5>
+                        <p>Copyright © 2022 Fidelma</p>
+                    </div>
                     
                 </div>
             </>
