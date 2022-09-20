7f60ce5b36cf11d1accedbb44ab9e144c95fab8a
Steph-PO8 <wendiying0311@gmail.com>
2022-09-20 20:14:46 +1000

    Add interaction between shopping cart and confirm page, all added dishes will be displayed on the confirm page with each of its price and the total price

--- react-frontend/src/components/ConfirmComponent.js
+++ react-frontend/src/components/ConfirmComponent.js
@@ -5,6 +5,14 @@ class ConfirmComponent extends React.Component{
 
     constructor(props){
         super(props)
+        this.state = {
+            quantity: 1,
+            foods : [], 
+            type : null,
+            foodsInCart : [], 
+            foodsCountInCart : [], 
+            cartOpen : false
+        }
     }
 
     testPost(){
@@ -12,7 +20,24 @@ class ConfirmComponent extends React.Component{
     }
 
     componentDidMount(){
-        console.log(this.props.location.state)
+        console.log(this.props.location.state);
+        console.log(this.props.location.state[0]);
+        console.log(this.props.location.state[0][1].name);
+        console.log(this.state.foodsInCart);
+    }
+
+    backToTypeMenu(){
+        let type = this.props.location.state[this.props.location.state.length-1]
+        this.props.history.push("/customer/menu/chicken",[this.props.location.state[0], type])
+    }
+
+    calculateTotalPrice(){
+        let totalPrice = 0;
+        for (let i = 0; i < this.props.location.state[0].length; i++){
+            let dishInCart = this.props.location.state[0][i]
+            totalPrice += dishInCart.price * dishInCart.quantity
+        }
+        return totalPrice
     }
 
     render(){
@@ -20,7 +45,7 @@ class ConfirmComponent extends React.Component{
             <>
                 <div>
                     <div className = "confirmHead">
-                        <img className = "orderBackButton" src = "/res/images/back.svg" />
+                        <img className = "orderBackButton" src = "/res/images/back.svg" onClick={() => this.backToTypeMenu()}/>
                         <h4>Fidelma</h4>
                     </div>
                     <div className = "orderTitle">
@@ -35,7 +60,18 @@ class ConfirmComponent extends React.Component{
                     </div>
                     <hr className = "confirmSeparateLine"/>
                     <div className = "orderDetail">
-                        <div className = "grid-container">
+                        {this.props.location.state[0].map((dish) =>(
+                            <div className = "grid-container" key = {dish.id}>
+                                <div className = "item1">
+                                    <img className = "gridPic" src="/res/images/beef3.jpg"/>
+                                </div>
+                                <div className = "item2"><strong>{dish.name}</strong></div>
+                                <div className = "item3">add-on details: xxxx</div>
+                                <div className = "item4"><strong>x{dish.quantity}</strong></div>
+                                <div className = "item5"><strong>${dish.price*dish.quantity}</strong></div>    
+                            </div>
+                        ))}
+                        {/* <div className = "grid-container">
                             <div className = "item1">
                                 <img className = "gridPic" src="/res/images/beef3.jpg"/>
                             </div>
@@ -52,16 +88,7 @@ class ConfirmComponent extends React.Component{
                             <div className = "item3">add-on details: xxxx</div>
                             <div className = "item4"><strong>x1</strong></div>
                             <div className = "item5"><strong>$11.8</strong></div>
-                        </div>
-                        <div className = "grid-container">
-                            <div className = "item1">
-                                <img className = "gridPic" src="/res/images/beef3.jpg"/>
-                            </div>
-                            <div className = "item2"><strong>Classic Beef Burger</strong></div>
-                            <div className = "item3">add-on details: xxxx</div>
-                            <div className = "item4"><strong>x1</strong></div>
-                            <div className = "item5"><strong>$11.8</strong></div>
-                        </div>
+                        </div> */}
                         <div className = "addComment">
                             <div className = "commentTitle">
                                 <h4>Put additional comment here:</h4>
@@ -72,7 +99,7 @@ class ConfirmComponent extends React.Component{
                     <div className = "finishOrder">
                         <div className = "totalCost">
                             <h4><strong>Total:</strong></h4>
-                            <h4><strong>$70.8</strong></h4>
+                            <h4><strong>${this.calculateTotalPrice()}</strong></h4>
                         </div> 
                         <button className = "finishOrderButton" onClick={()=>this.props.history.push("/submitPage",this.props.location.state)}>Order Now</button>
                         
