c8469274c29e2a77455cea3fdb2fd701235bd715
Steph-PO8 <wendiying0311@gmail.com>
2022-09-21 17:26:52 +1000

    Update income page

--- react-frontend/src/components/StaffIncomeComponent.js
+++ react-frontend/src/components/StaffIncomeComponent.js
@@ -5,6 +5,10 @@ class StaffIncomeComponent extends React.Component{
 
     constructor(props){
         super(props)
+
+        this.state = {
+            income:'100,000',
+        }
     }
 
     testPost(){
@@ -15,7 +19,21 @@ class StaffIncomeComponent extends React.Component{
         return(
             <>
                 <div>
-                    
+                    <div className = "incomeHead">
+                        <img className = "historyBackIcon" src="/res/images/back.svg" onClick={()=>window.location.href="/staff/dashboard"}/>
+                        <h4>Total Income</h4>
+                        <hr className = "historySeparateLine"/>
+                    </div>
+                    <div className = "incomeIconContainer">
+                        <img src="/res/images/income.png" />
+                        <h4>My Income:</h4>
+                    </div>
+                    <div className = "totalIncome">
+                        <h4><strong>${this.state.income}</strong></h4>
+                    </div>
+                    <div className = "withdrawContainer">
+                        <button className = "withdrawButton">Withdraw</button>
+                    </div>
 
                 </div>
             </>
