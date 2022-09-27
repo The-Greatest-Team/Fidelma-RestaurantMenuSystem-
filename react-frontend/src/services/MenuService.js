f39d210140b32e13a57a5e78802ce47b5bde119c
zhzizhou <zhzizhou@gmail.com>
2022-09-27 17:25:42 +1000

    updated constant apis

--- react-frontend/src/services/MenuService.js
+++ react-frontend/src/services/MenuService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+import {API_URL} from "../constant";
 
-const USERS_REST_API_URL = 'http://localhost:8080/staff/menu/';
+const USERS_REST_API_URL = API_URL + '/staff/menu/';
 
 class UserService{
 
