f432c19923b7c4cf621011e68e3aa850c28d70e0
zhzizhou <zhzizhou@gmail.com>
2022-10-01 21:40:56 +1000

    updated config file for hosting address

--- react-frontend/src/constant.jsx
+++ react-frontend/src/constant.jsx
@@ -1,2 +1,2 @@
 export const API_URL = process.env.REACT_APP_API_URL
-export const API_HEROKU = "http://fidelma-springboot.herokuapp.com"
+export const API_HEROKU = "https://fidelma-springboot.herokuapp.com"
